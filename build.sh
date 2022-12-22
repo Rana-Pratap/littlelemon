6cc6b64308827f4f3ff9b4d5e839bc80e1fc07cb
Rana Pratap Singh <108171374+Rana-Pratap@users.noreply.github.com>
2022-12-22 16:15:43 +0530

    python3.11 to py in build.sh

--- build.sh
+++ build.sh
@@ -2,11 +2,11 @@
 
 # Build the project
 echo "Building th project..."
-python3.11 -m pip install -r requirements.txt
+py -m pip install -r requirements.txt
 
 echo "Make Migrations..."
-python3.11 manage.py makemigrations --noinput
-python3.11 manage.py migrate --noinput
+py manage.py makemigrations --noinput
+py manage.py migrate --noinput
 
 echo "Collect Static..."
-python3.11 manage.py collectstatic --noinput --clear
+py manage.py collectstatic --noinput --clear
